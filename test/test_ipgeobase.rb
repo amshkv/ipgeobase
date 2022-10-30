00d05b3edd4b17facc7d71ee5f0b7283d70df1c1
Andrey Moshkov <fakesupp@gmail.com>
2022-10-30 20:54:12 +0300

    :recycle: improve test code

--- test/test_ipgeobase.rb
+++ test/test_ipgeobase.rb
@@ -4,16 +4,17 @@ require 'test_helper'
 
 class TestIpgeobase < Minitest::Test
   def test_full_data
-    url = 'http://ip-api.com/xml/8.8.8.8?fields=country,countryCode,city,lat,lon'
+    ip = '8.8.8.8'
+    url = "http://ip-api.com/xml/#{ip}?fields=country,countryCode,city,lat,lon"
     response = load_fixture('response')
     stub_request(:get, url).to_return(body: response)
 
-    ip_meta = Ipgeobase.lookup('8.8.8.8')
+    ip_meta = Ipgeobase.lookup(ip)
 
-    assert_equal 'Ashburn', ip_meta.city
-    assert_equal 'United States', ip_meta.country
-    assert_equal 'US', ip_meta.countryCode
-    assert_equal 39.03, ip_meta.lat
-    assert_equal -77.5, ip_meta.lon # rubocop:disable Lint/AmbiguousOperator
+    assert_equal('Ashburn', ip_meta.city)
+    assert_equal('United States', ip_meta.country)
+    assert_equal('US', ip_meta.countryCode)
+    assert_equal(39.03, ip_meta.lat)
+    assert_equal(-77.5, ip_meta.lon)
   end
 end
